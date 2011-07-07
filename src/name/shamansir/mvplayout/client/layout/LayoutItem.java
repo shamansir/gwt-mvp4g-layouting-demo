2456784993e70f191a5e2ec3dd0df3b8a4ff0a22
Ulric Wilfred <shaman.sir@gmail.com>
2011-07-08 00:56:46 +0300

    removed state from item layout, user/info shown ok

--- src/name/shamansir/mvplayout/client/layout/LayoutItem.java
+++ src/name/shamansir/mvplayout/client/layout/LayoutItem.java
@@ -3,9 +3,8 @@ package name.shamansir.mvplayout.client.layout;
 import name.shamansir.mvplayout.client.id.L;
 import name.shamansir.mvplayout.client.id.O;
 import name.shamansir.mvplayout.lib.ui.IsOutlet;
-import name.shamansir.mvplayout.lib.ui.state.LayoutWithState;
-import name.shamansir.mvplayout.lib.ui.state.State;
 import name.shamansir.mvplayout.lib.ui.structure.Place;
+import name.shamansir.mvplayout.lib.ui.widget.Layout;
 import name.shamansir.mvplayout.lib.ui.widget.Outlet;
 
 import com.google.gwt.core.client.GWT;
@@ -14,7 +13,7 @@ import com.google.gwt.uibinder.client.UiField;
 import com.google.gwt.uibinder.client.UiTemplate;
 import com.google.gwt.user.client.ui.Widget;
 
-public class LayoutItem extends LayoutWithState {
+public class LayoutItem extends Layout {
 
     @UiTemplate("LayoutItem.ui.xml")
     interface LayoutItemUiBinder extends UiBinder<Widget, LayoutItem> { }
@@ -22,11 +21,10 @@ public class LayoutItem extends LayoutWithState {
     
     @UiField Outlet placeA;
     @UiField Outlet placeB;
-    @UiField Outlet placeC;   
-    @UiField Outlet status;
+    @UiField Outlet placeC;
     
     public LayoutItem() {
-        super(L.ITEM, new Place[] { O.A, O.B, O.C }, O.STATUS);
+        super(L.ITEM, new Place[] { O.A, O.B, O.C });
 
         initWidget(uiBinder.createAndBindUi(this));     
     }
@@ -37,26 +35,8 @@ public class LayoutItem extends LayoutWithState {
         if (place.equals(O.A)) return placeA;
         if (place.equals(O.B)) return placeB;
         if (place.equals(O.C)) return placeC;
-        if (place.equals(O.STATUS)) return status;
         throw new IndexOutOfBoundsException("No widget position at place " + place + " for layout " + id());
     }
 
-    @Override
-    public void prepare(State state) {
-        switch (state) {
-            case NO_DATA: case LOADING_DATA: case NO_MATCHES: {
-                placeA.setVisible(false);
-                placeB.setVisible(false);
-                placeC.setVisible(false);
-                status.setVisible(true);
-            } break;
-            case HAS_DATA: {
-                status.setVisible(false);                
-                placeA.setVisible(true);
-                placeB.setVisible(true);
-                placeC.setVisible(true);                
-            }
-        }        
-    }
 }
 
